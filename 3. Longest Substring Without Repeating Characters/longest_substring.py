8e410655483c60471b93f03935d63e2d416682e5
tamer-dr <tamerdaoud247@gmail.com>
2025-10-01 18:04:31 +0100

    update 3rd problem solution

--- 3. Longest Substring Without Repeating Characters/longest_substring.py
+++ 3. Longest Substring Without Repeating Characters/longest_substring.py
@@ -6,11 +6,20 @@ class Solution:
         char_index = {}
         left = 0
         max_length = 0
+        best_substring = ""
 
         for right, char in enumerate(s):
             if char in char_index and char_index[char] >= left:
                 left = char_index[char] + 1
             char_index[char] = right
-            max_length = max(max_length, right - left + 1)
+
+            # calcul longueur actuelle
+            current_length = right - left + 1
+            if current_length > max_length:
+                max_length = current_length
+                best_substring = s[left:right+1]
+
+        print("Sous-chaîne trouvée :", best_substring)
+        print("Longueur :", max_length)
 
         return max_length
--- 3. Longest Substring Without Repeating Characters/test.py
+++ 3. Longest Substring Without Repeating Characters/test.py
@@ -3,4 +3,3 @@ from longest_substring import Solution
 if __name__ == "__main__":
     s = input("Entrez une chaîne de caractères : ")
     result = Solution().lengthOfLongestSubstring(s)
-    print("Longueur du plus long substring sans répétition :", result)
