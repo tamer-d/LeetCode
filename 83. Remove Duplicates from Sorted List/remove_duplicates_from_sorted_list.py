909144951cf7c25b03396ace6fd51455df1bc306
tamer-dr <tamerdaoud247@gmail.com>
2025-10-06 08:05:35 +0100

    83

---
+++ 83. Remove Duplicates from Sorted List/remove_duplicates_from_sorted_list.py
@@ -0,0 +1,19 @@
+# LeetCode Problem: 83. Remove Duplicates from Sorted List
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        current = head
+        
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return head
---
+++ 83. Remove Duplicates from Sorted List/test.py
@@ -0,0 +1,26 @@
+from remove_duplicates_from_sorted_list import ListNode, Solution
+
+def build_list(values):
+    head = ListNode(values[0])
+    curr = head
+    for v in values[1:]:
+        curr.next = ListNode(v)
+        curr = curr.next
+    return head
+
+def print_list(head):
+    vals = []
+    while head:
+        vals.append(head.val)
+        head = head.next
+    print(vals)
+
+if __name__ == "__main__":
+    values = list(map(int, input("Entrez les valeurs triées de la liste séparées par des espaces : ").split()))
+    
+    head = build_list(values)
+    sol = Solution()
+    new_head = sol.deleteDuplicates(head)
+    
+    print("Liste après suppression des doublons :")
+    print_list(new_head)
